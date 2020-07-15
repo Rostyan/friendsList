5349dc03597fd25646d981e0cd7aa2980449a833
Rostyan <kondrat1310@gmail.com>
2020-07-15 12:24:36 +0300

    added new server side code

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,7 +5,7 @@ const { ensureAuthenticated } = require('../config/auth');
 const passport = require('passport');
 
 router.get('/', ensureAuthenticated, function (req, res) {
-  
+
   res.send({
     name: req.session.passport.user.name,
     email: req.session.passport.user.email,
@@ -14,31 +14,75 @@ router.get('/', ensureAuthenticated, function (req, res) {
 
 router.get('/friend', function (req, res) {
   User.find({})
-  
-  .then(user => {
-    let usersData = user.map((user) => {
-      return {email: user.email, 
-              friendsList: req.session.passport.user.friendsList,
-              name: user.name,
-              status: user.status
-            };
-    });
-    res.send(usersData);
-})
+
+    .then(user => {
+      let usersData = user.map((user) => {
+        return {
+          email: user.email,
+          friendsList: req.session.passport.user.friendsList,
+          name: user.name,
+          status: user.status
+        };
+      });
+      res.send(usersData);
+    })
 
 
 })
 
 router.post('/addfriend', function (req, res) {
-  
+  let updatedOptions = {
+    name: req.body.name,
+    email: req.body.email,
+    status: 'friends'
+  };
+
+  User.findOneAndUpdate(
+    { _id: req.body.id },
+    { $push: { friendsList: updatedOptions } },
+    function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(success);
+      }
+    });
 })
 
 router.post('/removefriend', function (req, res) {
+  let updatedOptions = {
+    name: req.body.name,
+    email: req.body.email,
+    status: ' '
+  };
 
+  User.findOneAndUpdate({ _id: req.params.id }, { $set: updatedOptions }, function (error) {
+    if (error) {
+      res.send(error);
+    } else {
+      res.redirect('back')
+    }
+  });
+})
+
+router.post('/ignorefriend', function (req, res) {
+  let updatedOptions = {
+    name: req.body.name,
+    email: req.body.email,
+    status: 'pending request'
+  };
+
+  User.findOneAndUpdate({ _id: req.params.id }, { $set: updatedOptions }, function (error) {
+    if (error) {
+      res.send(error);
+    } else {
+      res.redirect('back')
+    }
+  });
 })
 
 router.get('/login', (req, res) => {
-  res.send({message: 'OK!'});
+  res.send({ message: 'OK!' });
 });
 
 router.post('/login', (req, res, next) => {
@@ -60,12 +104,12 @@ router.post('/signup', function (req, res) {
     email: req.body.email,
     password: req.body.password,
     name: req.body.name
-    });
+  });
 
-    User.create(newUser, function (err, user) {
-      if (err) throw err;
-      console.log(user);
-    });
+  User.create(newUser, function (err, user) {
+    if (err) throw err;
+    console.log(user);
+  });
 
 
   res.redirect('/login');
